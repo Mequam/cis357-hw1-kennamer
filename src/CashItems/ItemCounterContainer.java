7ac787e49d19ea6bca571c5134e6a2260c03fba5
Mequam <blue9ja@gmail.com>
2022-07-03 10:43:14 -0400

    added basic i/o to the program and array sorting inside of the ItemCounterContainer

--- src/CashItems/ItemCounterContainer.java
+++ src/CashItems/ItemCounterContainer.java
@@ -2,6 +2,8 @@ package CashItems;
 
 import Monoids.CounterContainer;
 
+import java.util.Arrays;
+
 public class ItemCounterContainer extends Monoids.CounterContainer {
     /*
      * java appears to have very ugly higher order functions
@@ -19,6 +21,22 @@ public class ItemCounterContainer extends Monoids.CounterContainer {
         for (int i = 0 ; i < item_list.length; i++) {
             data[i] = new ItemCounter(item_list[i]);
         }
+
+        //sort the data array by name
+        Arrays.sort(data,(a,b) -> {return ((Item)a.element).getItemName().compareTo(((Item)b.element).getItemName());});
+
+        for (ItemCounter c : (ItemCounter []) data)
+        {
+            System.out.println(((Item)c.element).getItemName());
+        }
+    }
+    /** returns true if the object types contain the given id*/
+    public boolean contains(int id) {
+        return  !java.util.Objects.isNull(
+                    testMap(
+                        (i) -> {return ((Item)((ItemCounter)(i)).element).getItemCode() == id;},
+                        (j)->{ return true;} )
+            );//if we return anything from our generic array search, then we are not null and we need to be true
     }
     /** Gets the amount of the given item type*/
     public  int get_type_amount(int id) {
--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,23 +1,79 @@
 import CashItems.*;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CashRegister {
+
+    public static void say(String s,String format) {
+            System.out.print(String.format(format,s));
+    }
+    public static void say(String s) {
+        say(s,"%-26s");
+    }
+
+    /**queries the user for information using the proper formating*/
+    public  static String ask(String question,String formatting) {
+        say(question,formatting);
+        Scanner inScan = new Scanner(System.in);
+        return inScan.nextLine();
+    }
+    public static String ask(String question) {
+        return ask(question,"%-26s");
+    }
+    public static int askInt(String question,String angry_question) {
+        String a = ask(question);
+        boolean good_responce = false;
+        int ret_val = 0;
+        while (!good_responce) {
+            try {
+                ret_val = Integer.parseInt(a);
+                good_responce = true;
+            }
+            catch (Exception e) {
+                a = ask(angry_question);
+            }
+        }
+        return ret_val;
+    }
+
+    public  static int askInt(String question) {
+        return askInt(question,"Enter a valid integer:");
+    }
+    /**asks the user for a product code*/
+    public  static int askProductCode(ItemCounterContainer icc) {
+        int id = askInt("Enter Product Code:");
+        while (!icc.contains(id)) {
+            id = askInt("Enter Valid Product Code:");
+        }
+        return id;
+    }
     public static void main(String[] args) {
         System.out.println(args[0]);
+        System.out.println("Welcome to Kennamer cash register system!\n");
 
-       ItemCounterContainer icc = new ItemCounterContainer(args[0]);
-        Item [] items = Item.gen_item_list(args[0]);
+        ItemCounterContainer icc = new ItemCounterContainer(args[0]);
+        Scanner inScan = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            System.out.print("Begin a new sale? (Y/N)");
+            String userInput = inScan.nextLine();
+            if (userInput.toUpperCase().equals("Y")) {
+                //demand a valid product code
+                int productCode = askProductCode(icc);
 
-        System.out.println(items[0].getItemName());
+                //print out to the screen in a nice formated way
+                say("item name: ","%26s");
+                say(icc.get_type_name(productCode),"%s\n");
+                int amount = askInt("Enter Quantity: ");
 
-        System.out.println(icc.get_type_name(2));
-        System.out.println(icc.get_type_amount(2));
-        System.out.println(icc.get_type_cost(2));
+                //actually store the increase in amount
+                icc.get_counter(productCode).count += amount;
 
-
-        System.out.println(icc.get_item(2).getItemName());
-        System.out.println(icc.get_item(2).getItemCode());
-        System.out.println(icc.get_item(2).getUnitPrice());
-
-        System.out.println("Hello World");
+                double itemTotal = icc.get_type_cost(productCode)*amount;
+                say("item total: ","%26s");
+                say(String.format("$%.02f",itemTotal),"%s\n");
+            }
+        }
     }
 }
