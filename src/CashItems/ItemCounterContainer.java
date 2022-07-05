c256d8c58768d2f92b78ba71c7e5e2f035dc00fa
Mequam <blue9ja@gmail.com>
2022-07-05 19:47:05 -0400

    Revert "updated the default tax rate to match the one in the assignment"
    This reverts commit 9b14c4c0f3319b5d4f85d375e9d08e764ad052d0.
    
    looks like there is an error in the assignment program output
    with the sales tax calculation,
    
    we were correct to use 0.06 for our tax percentage and should
    NOT have changed

--- src/CashItems/ItemCounterContainer.java
+++ src/CashItems/ItemCounterContainer.java
@@ -70,7 +70,7 @@ public class ItemCounterContainer extends Monads.CounterContainer {
      * @return the subtotal with a 6% tax rate
      */
     public double taxedCost(){
-        return  taxedCost(0.006);
+        return  taxedCost(0.06);
     }
 
     /**
