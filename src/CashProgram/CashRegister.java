327ca33b9f93a1e319adb8f9ea94a42ff6265138
Mequam <blue9ja@gmail.com>
2022-07-05 19:10:59 -0400

    added the seperator after asking if they want to continue it had to be added on seperate branches :( note to self make sure everything is working before branching new features :D

--- src/CashProgram/CashRegister.java
+++ src/CashProgram/CashRegister.java
@@ -184,8 +184,9 @@ public class CashRegister {
             //ensure that we start with no data in the container
             icc.zeroData();
             System.out.print(String.format("\n%-26s","Begin a new sale? (Y/N) "));
-
             String userInput = inScan.nextLine();
+
+            printSeperator();
 
             if (userInput.toUpperCase().equals("Y")) {
                 //demand a valid product code
